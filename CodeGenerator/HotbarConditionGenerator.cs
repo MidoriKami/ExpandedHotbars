f10477b45ed7eca7e98a767c83882db7587c9271
MidoriKami <mi_kami@outlook.com>
2026-09-13 20:10:53 -0700

    Add Code Generator

---
+++ CodeGenerator/HotbarConditionGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace CodeGenerator;
+
+[Generator]
+public class HotbarConditionGenerator : JsonMappingGenerator {
+    protected override string TargetType => "ConditionBase";
+    protected override string BaseNameSpace => "ExpandedHotbars.Conditions";
+}
